f5e37e004c75db39e5191c6c1cdf207f359fd21c
Par2ival <2ndhandhacker@gmail.com>
2022-11-24 21:53:39 +1100

    Update DiSz_Button_Toggle.ino

--- DiSz_Button_Toggle.ino
+++ DiSz_Button_Toggle.ino
@@ -9,16 +9,17 @@ void setup() {
 void loop() {
   int button_state = digitalRead(Toggle_pin);
 
-// NOTE: The button State will need to the opposite for each board.
+// NOTE: The button State will need to the opposite for the code loaded on each board.
 //       i.e S2 mini: button_state == HIGH
 //           D1 mini: button_state == LOW
 
   if (button_state == HIGH) {
+    delay_value = 1000;
 
     //button press's will be acknowledged and executed;
 
   } else {
-
+    delay_value = 100;
     delay(delay_value);
 
   }
